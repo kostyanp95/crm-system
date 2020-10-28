37c6c1c9695ea2e978c2fe0e8c9c7f8233dadcf1
Kostyanp95 <kostyanp95@yandex.ru>
2020-10-29 01:35:34 +0300

    feat(app, package): added cors and morgan packages

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require("morgan");
+const cors = require("cors");
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
@@ -7,8 +9,10 @@ const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 const app = express()
 
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extend: true}))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
