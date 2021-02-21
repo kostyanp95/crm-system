91f8fe1d7129b8d09c22801babc522e21634a09b
Kostyanp95 <kostyanp95@yandex.ru>
2021-02-21 03:07:46 +0300

    feat(auth.js): add login and register endpoints

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,42 +1,75 @@
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
+const keys = require('../config/keys')
 const User = require('../models/User')
 
-module.exports.login = function(req, res) {
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
+/**
+ * Login
+ * @param req - request
+ * @param res - response
+ * @returns {Promise<void>}
+ */
+module.exports.login = async function (req, res) {
+    const candidate = await User.findOne({ email: req.body.email })
+
+    if (candidate) {
+        // Проверка пароля
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if (passwordResult) {
+            // Генерация токена, пароли совпали
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, { expiresIn: 3600 })
+            res.status(200).json({
+                token: token
+            })
+        } else {
+            // Пароли не совпали
+            res.status(401).json({
+                message: 'Пароли не совпадают...'
+            })
+        }
+    } else {
+        // Пользователь не существует
+        res.status(404).json({
+            message: 'Пользователь с таким email не найден.'
+        })
     }
-  })
 }
 
-module.exports.register = async function(req, res) {
-  const candidate = await User.findOne({email: req.body.email})
+/**
+ * User registration
+ * @param req - request
+ * @param res - response
+ * @returns {Promise<void>}
+ */
+module.exports.register = async function (req, res) {
+    const candidate = await User.findOne({ email: req.body.email })
 
-  // если юзер существует
-  if(candidate) {
-    // возвращаем ошибку
-    res.status(409).json({
-      message: 'Такой email уже занят.'
-    })
-  } else {
-    // иначе, создаем юзера
-    const salt = bcrypt.genSaltSync(10)
-    const password = req.body.password
-    const user = new User({
-      email: req.body.email,
-      password: bcrypt.hashSync(password, salt)
-    })
+    // если юзер существует
+    if (candidate) {
+        // возвращаем ошибку
+        res.status(409).json({
+            message: 'Такой email уже занят.'
+        })
+    } else {
+        // иначе, создаем юзера
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
 
-    try {
-      // созраняем пользователя
-      await user.save()
-      res.status(201).json(user)
-    } catch (e) {
-      // обрабатываем ошибку
+        try {
+            // сохраняем пользователя
+            await user.save()
+            res.status(201).json(user)
+        } catch (e) {
+            // обрабатываем ошибку
+
+        }
 
     }
-
-  }
 }
