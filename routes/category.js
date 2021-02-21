441d49ad2a284fb69e7821dbed736bc89de79df9
Kostyanp95 <kostyanp95@yandex.ru>
2021-02-21 14:30:22 +0300

    feat(category.js): add guard to category route

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const passport = require("passport");
+
 const controller = require('../controllers/category')
 const router = express.Router()
 
-router.get('/', controller.getAll)
+router.get('/', passport.authenticate('jwt', { session: false }), controller.getAll)
 router.get('/:id', controller.getById)
 router.delete('/:id', controller.remove)
 router.post('/', controller.create)
