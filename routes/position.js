816b4814c29409a6c67fbc013f5c172ec18ae48d
Kostyanp95 <kostyanp95@yandex.ru>
2021-02-27 16:11:11 +0300

    feat(position controller): implemented all methods

--- routes/position.js
+++ routes/position.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const controller = require('../controllers/position')
 const router = express.Router()
+
+const controller = require('../controllers/position')
 
 router.get('/:categoryId', controller.getByCategoryId)
 router.post('/', controller.create)
