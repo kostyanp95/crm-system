3e293aceac3b7916a11aee8c9dff69fe066a39e6
Kostyanp95 <kostyanp95@yandex.ru>
2021-02-27 15:46:23 +0300

    feat(controllers): implemented getByCategoryId method

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,44 @@
-module.exports.getByCategoryId = function(req, res) {
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
 
+/**
+ * Get all positions
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+module.exports.getByCategoryId = async function (req, res) {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        })
+        res.status(200).json(positions)
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.create = function(req, res) {
+module.exports.create = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.remove = function(req, res) {
+module.exports.remove = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.update = function(req, res) {
+module.exports.update = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
--- models/Position.js
+++ models/Position.js
@@ -1,4 +1,5 @@
-const  mongoose = require('mongoose')
+const mongoose = require('mongoose')
+
 const Schema = mongoose.Schema
 
 const positionSchema = new Schema({
@@ -18,7 +19,6 @@ const positionSchema = new Schema({
         ref: 'users',
         type: Schema.Types.ObjectId
     }
-
 })
 
 module.exports = mongoose.model('positions', positionSchema)
