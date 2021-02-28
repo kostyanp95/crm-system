848ef13470467810885b226886558c9c14504223
Kostyanp95 <kostyanp95@yandex.ru>
2021-02-28 16:05:20 +0300

    feat(routes): add guards for routers

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const controller = require('../controllers/auth')
 const router = express.Router()
+
+const controller = require('../controllers/auth')
 
 // localhost:5000/api/auth/login
 router.post('/login', controller.login)
--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const controller = require('../controllers/order')
+const passport = require("passport")
 const router = express.Router()
 
+const controller = require('../controllers/order')
 
-router.get('/', controller.getAll)
-router.post('/', controller.create)
+router.get('/', passport.authenticate('jwt', { session: false }), controller.getAll)
+router.post('/', passport.authenticate('jwt', { session: false }), controller.create)
 
 
 module.exports = router
