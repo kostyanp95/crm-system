63c4691fa6892296ea449a95b0fd53cafceecf1d
Kostyanp95 <kostyanp95@yandex.ru>
2021-02-28 16:05:20 +0300

    feat(routes): add guards for routers

--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const passport = require("passport");
+const passport = require("passport")
 const router = express.Router()
 
 const controller = require('../controllers/category')
