171f36163b5215b53ae8637aa0026b5820b68951
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-06 17:41:09 +0300

    feat(client): create register-page, auth & site layout with routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,35 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
+import {SiteLayoutComponent} from "./shared/layouts/site-layout/site-layout.component";
 import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
 
 const routes: Routes = [
   {
+    path: "",
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: "",
+        redirectTo: '/login',
+        pathMatch: 'full'
+      },
+      {
+        path: "login",
+        component: LoginPageComponent,
+      },
+      {
+        path: "register",
+        component: RegisterPageComponent,
+      },
+    ]
+  },
+  {
     path: "login",
-    component: LoginPageComponent
-  }
+    component: SiteLayoutComponent,
+    children: []
+  },
 ];
 
 @NgModule({
