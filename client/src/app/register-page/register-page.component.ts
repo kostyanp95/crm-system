32254f0410efa0713ea1df23da57c21c517f8078
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-11 00:44:10 +0300

    feat(register-page): complete registration

--- client/src/app/register-page/register-page.component.ts
+++ client/src/app/register-page/register-page.component.ts
@@ -1,16 +1,49 @@
-import {Component, OnInit} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Subscription } from "rxjs";
+import { Router } from "@angular/router";
+
+import { AuthService } from "../shared/services/auth.service";
+import { User } from "../shared/models/interfaces";
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  styleUrls: ['./register-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RegisterPageComponent implements OnInit {
+export class RegisterPageComponent implements OnInit, OnDestroy {
+  form: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(7)]]
+  })
+  subscription: Subscription
 
-  constructor() {
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.form.disable()
+    const user: User = this.form.getRawValue()
+    this.subscription = this.authService.register(user)
+      .subscribe(
+        () => this.router.navigate(['/login'], {
+          queryParams: {
+            registered: true
+          }
+        }),
+        error => {
+          console.warn(error)
+          this.form.enable()
+        })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription ? this.subscription.unsubscribe() : null
+  }
 }
