c946fb846f6aae6024586183460720b3d2a935ea
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-11 00:26:38 +0300

    feat(login-page): implemented login method

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,23 +1,55 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Subscription } from "rxjs";
+
+import { AuthService } from "../shared/services/auth.service";
+import { User } from "../shared/models/interfaces";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
   form: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(7)]]
   });
+  subscription: Subscription
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams
+      .subscribe((params: Params) => {
+        if (params['registered']) {
+          // you can login
+        } else if (params['accessDenied']) {
+          // nee auth
+        }
+      })
   }
 
   onSubmit(): void {
+    this.form.disable()
+    const user: User = this.form.getRawValue()
+    this.subscription = this.authService.login(user)
+      .subscribe(
+        () => this.router.navigate(['/overview']),
+        error => {
+          console.log(error)
+          this.form.enable()
+        }
+      )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription ? this.subscription.unsubscribe() : null
   }
 }
