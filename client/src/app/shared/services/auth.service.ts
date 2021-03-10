27034a79b292a7fffb2a9e4a99b0a87153224f8b
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-11 00:44:10 +0300

    feat(register-page): complete registration

--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
   }
 
   register(user: User): Observable<User> {
-    return this.http.post<User>('api/auth/register', user)
+    return this.http.post<User>('/api/auth/register', user)
   }
 
   login(user: User): Observable<{ token: string }> {
