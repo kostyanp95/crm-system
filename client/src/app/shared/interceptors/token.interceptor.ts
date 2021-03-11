567d2eb702cf02f3a52619e6e784ebc26f641a87
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-12 01:27:45 +0300

    fix(token.interceptor): add handleAuthError if the token has expired

--- client/src/app/shared/interceptors/token.interceptor.ts
+++ client/src/app/shared/interceptors/token.interceptor.ts
@@ -1,13 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -18,6 +21,20 @@ export class TokenInterceptor implements HttpInterceptor {
         }
       })
     }
-    return next.handle(req);
+    return next.handle(req)
+      .pipe(
+        catchError((error: HttpErrorResponse) => this.handleAuthError(error))
+      )
+  }
+
+  private handleAuthError(error: HttpErrorResponse): Observable<any> {
+    if (error.status === 401) {
+      this.router.navigate(['/login'], {
+        queryParams: {
+          sessionFailed: true
+        }
+      })
+    }
+    return throwError(error)
   }
 }
