13163c38c71b6112c57f9bd90156ac455b76d7ab
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-17 00:27:17 +0300

    refactor(category-page): refactor categories

--- client/src/app/categories-page/categories-page.component.ts
+++ client/src/app/categories-page/categories-page.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 import { CategoriesService } from "../shared/services/categories.service";
 import { Category } from "../shared/models/category.model";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-categories-page',
@@ -11,19 +12,13 @@ import { Category } from "../shared/models/category.model";
 })
 export class CategoriesPageComponent implements OnInit {
 
-  loading: boolean = false
-  private categories: Array<Category> = []
+  categories$: Observable<Array<Category>>
 
   constructor(private categoriesService: CategoriesService) {
   }
 
   ngOnInit(): void {
-    this.categoriesService.fetch()
-      .subscribe(cat => {
-        this.loading = false
-        this.categories = cat
-        console.log(cat)
-      })
+    this.categories$ = this.categoriesService.fetch()
   }
 
 }
