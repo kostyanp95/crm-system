795318f87fb4d30126e76fcaab5deff3b321f8f3
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-20 15:08:00 +0300

    feat(categories-form): create form for add and edit positions

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,5 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CategoriesService } from "../../shared/services/categories.service";
+import { switchMap } from "rxjs/operators";
+import { of } from "rxjs";
+import { MaterializeService } from "../../shared/services/materialize.service";
 
 @Component({
   selector: 'app-categories-form',
@@ -9,21 +14,58 @@ import { ActivatedRoute, Params } from "@angular/router";
 })
 export class CategoriesFormComponent implements OnInit {
 
+  form: FormGroup = this.fb.group({
+    name: [null, [Validators.required]]
+  })
   isNew: boolean = true
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private fb: FormBuilder,
+              private categoriesService: CategoriesService) {
   }
 
   ngOnInit(): void {
+    // this.route.params
+    //   .subscribe((params: Params) => {
+    //     if (params['id']) {
+    //       // Edit form
+    //       this.isNew = false
+    //     } else {
+    //       // Create form
+    //     }
+    //   })
+
+    this.form.disable()
+
     this.route.params
-      .subscribe((params: Params) => {
-        if (params['id']) {
-          // Edit form
-          this.isNew = false
-        } else {
-          // Create form
-        }
-      })
+      .pipe(
+        switchMap(
+          (params: Params) => {
+            if (params['id']) {
+              this.isNew = false
+              console.log(params['id'])
+              console.log(params['name'])
+              return this.categoriesService.geById(params['id'])
+            }
+            return of(null)
+          }
+        )
+      )
+      .subscribe(
+        category => {
+          if (category) {
+            this.form.patchValue({
+              name: category.name
+            })
+            MaterializeService.updateTextFields()
+            this.form.enable()
+          }
+        },
+        error => MaterializeService.toast(error.error.message)
+      )
   }
 
+  onSubmit(): void {
+
+  }
 }
