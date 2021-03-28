5957aa2459e020817a34957163a72a912eba954d
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-28 05:59:45 +0300

    feat(categories-form): add create and update categories methods

--- client/src/app/shared/services/categories.service.ts
+++ client/src/app/shared/services/categories.service.ts
@@ -18,4 +18,22 @@ export class CategoriesService {
   geById(id: string): Observable<Category> {
     return this.http.get<Category>(`/api/category/${id}`)
   }
+
+  create(name: string, image?: File): Observable<Category> {
+    const formData = new FormData()
+
+    image ? formData.append('image', image, image.name) : null
+    formData.append('name', name)
+
+    return this.http.post<Category>('/api/category/', formData)
+  }
+
+  update(id: string, name: string, image?: File): Observable<Category> {
+    const formData = new FormData()
+
+    image ? formData.append('image', image, image.name) : null
+    formData.append('name', name)
+
+    return this.http.patch<Category>(`/api/category/${id}`, formData)
+  }
 }
