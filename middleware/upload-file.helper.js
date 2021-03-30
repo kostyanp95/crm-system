edea91ed44ab107a40d60b230f6f3c76a411c296
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-30 09:27:05 +0300

    refactor(upload-file.helper): remove file filter

--- middleware/upload-file.helper.js
+++ middleware/upload-file.helper.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
 })
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/png') {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
         cb(null, true)
     } else {
         cb(null, false)
@@ -25,6 +25,6 @@ const limits = {
 
 module.exports = multer({
     storage,
-    fileFilter,
+    // fileFilter,
     limits
 })
