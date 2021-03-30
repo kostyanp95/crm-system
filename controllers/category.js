52b2800188aa777339bb9e1d764961cb6b7c874a
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-30 09:30:18 +0300

    refactor(upload-file.helper): change limit size file

--- controllers/category.js
+++ controllers/category.js
@@ -20,18 +20,6 @@ module.exports.getById = async function (req, res) {
     }
 }
 
-module.exports.remove = async function (req, res) {
-    try {
-        await Category.remove({ _id: req.params.id })
-        await Position.remove({ category: req.params.id })
-        res.status(200).json({
-            message: 'Категория удалена'
-        })
-    } catch (e) {
-        errorHandler(res, e)
-    }
-}
-
 module.exports.create = async function (req, res) {
     const category = new Category({
         name: req.body.name,
@@ -64,6 +52,18 @@ module.exports.update = async function (req, res) {
             { new: true }
         )
         res.status(200).json(category)
+    } catch (e) {
+        errorHandler(res, e)
+    }
+}
+
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({ _id: req.params.id })
+        await Position.remove({ category: req.params.id })
+        res.status(200).json({
+            message: 'Категория удалена'
+        })
     } catch (e) {
         errorHandler(res, e)
     }
