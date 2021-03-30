e03d1c9fee91b8fbe0dee4b0d1c1b37a29163e9e
Kostyanp95 <kostyanp95@yandex.ru>
2021-03-30 09:58:01 +0300

    feat(positions): add and edit positions

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,4 +1,17 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+
+import { PositionService } from "../../../shared/services/position.service";
+import { Position } from "../../../shared/models/position.model";
+import { ModalMaterialInstance, MaterializeService } from "../../../shared/services/materialize.service";
 
 @Component({
   selector: 'app-positions-form',
@@ -6,12 +19,41 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./positions-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PositionsFormComponent implements OnInit {
+export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() {
+  @ViewChild('modal') modalRef: ElementRef
+  @Input('categoryId') categoryId
+  positions: Array<Position> = []
+  loading: boolean = false
+  modal: ModalMaterialInstance
+
+  constructor(private positionService: PositionService) {
   }
 
   ngOnInit(): void {
+    this.loading = true
+    this.positionService.fetch(this.categoryId)
+      .subscribe(positions => this.positions = positions)
+    this.loading = false
   }
 
+  ngAfterViewInit(): void {
+    this.modal = MaterializeService.initModal(this.modalRef)
+  }
+
+  onSelectPosition(position: Position): void {
+    this.modal.open()
+  }
+
+  onAddPosition(): void {
+    this.modal.open()
+  }
+
+  ngOnDestroy(): void {
+    this.modal.destroy()
+  }
+
+  onCancel(): void {
+    this.modal.close()
+  }
 }
