81946755c5f9a58bb2b251b29391728c8b230b3a
Kostyanp95 <kostyanp95@yandex.ru>
2021-04-04 13:59:13 +0300

    feat(position-form): add feature create, update and delete position

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,6 +1,7 @@
 import {
   AfterViewInit,
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   ElementRef,
   Input,
@@ -11,7 +12,8 @@ import {
 
 import { PositionService } from "../../../shared/services/position.service";
 import { Position } from "../../../shared/models/position.model";
-import { ModalMaterialInstance, MaterializeService } from "../../../shared/services/materialize.service";
+import { MaterializeService, ModalMaterialInstance } from "../../../shared/services/materialize.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-positions-form',
@@ -24,16 +26,35 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
   @ViewChild('modal') modalRef: ElementRef
   @Input('categoryId') categoryId
   positions: Array<Position> = []
+  positionId = null
   loading: boolean = false
   modal: ModalMaterialInstance
+  form: FormGroup
 
-  constructor(private positionService: PositionService) {
+  constructor(private positionService: PositionService,
+              private cdr: ChangeDetectorRef,
+              private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.loadingPositions()
+    this.initForm()
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      name: [null, [Validators.required]],
+      cost: [null, [Validators.required, Validators.min(1)]]
+    })
+  }
+
+  loadingPositions(): void {
     this.loading = true
     this.positionService.fetch(this.categoryId)
-      .subscribe(positions => this.positions = positions)
+        .subscribe(positions => {
+          this.positions = positions
+          this.cdr.markForCheck()
+        })
     this.loading = false
   }
 
@@ -42,11 +63,19 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
   }
 
   onSelectPosition(position: Position): void {
+    this.positionId = position._id
     this.modal.open()
+    this.form.patchValue({
+      name: position.name,
+      cost: position.cost
+    })
+    MaterializeService.updateTextFields()
   }
 
   onAddPosition(): void {
+    this.positionId = null
     this.modal.open()
+    this.form.reset()
   }
 
   ngOnDestroy(): void {
@@ -55,5 +84,71 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
 
   onCancel(): void {
     this.modal.close()
+  }
+
+  onSubmit(): void {
+    this.form.disable()
+
+    const newPosition: Position = {
+      name: this.form.value.name,
+      cost: this.form.value.cost,
+      category: this.categoryId
+    }
+
+    const completed = () => {
+      this.modal.close()
+      this.form.reset()
+      this.form.enable()
+    }
+
+    if (this.positionId) {
+      newPosition._id = this.positionId
+      this.positionService.update(newPosition)
+          .subscribe(
+            position => {
+              const index = this.positions.findIndex(p => p._id === position._id)
+              this.positions[index] = position
+              this.cdr.markForCheck()
+              MaterializeService.toast('Позиция изменена')
+            },
+            error => {
+              MaterializeService.toast(error.error.message)
+              console.log(error.error.message)
+            },
+            completed
+          )
+    } else {
+      this.positionService.create(newPosition)
+          .subscribe(
+            position => {
+              this.positions.push(position)
+              this.cdr.markForCheck()
+              MaterializeService.toast('Позиция создана')
+            },
+            error => {
+              MaterializeService.toast(error.error.message)
+              console.log(error.error.message)
+            },
+            completed
+          )
+    }
+  }
+
+  deletePosition(event: Event, position: Position) {
+    event.stopPropagation()
+    const decision = window.confirm(`Удалить позицию "${position.name}"?`)
+
+    if (decision) {
+      this.positionService.delete(position)
+          .subscribe(
+            response => {
+              const idx = this.positions.findIndex(p => p._id === position._id)
+              this.positions.splice(idx, 1)
+              this.cdr.markForCheck()
+              MaterializeService.toast(response.message)
+            },
+            error => MaterializeService.toast(error.error.message)
+          )
+    }
   }
 }
