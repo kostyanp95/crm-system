1623f3e642e0be6cdff0fe85882faf17543e5098
Kostyanp95 <kostyanp95@yandex.ru>
2021-04-04 13:58:11 +0300

    feat(position.service): add create, update and delete methods

--- client/src/app/shared/services/position.service.ts
+++ client/src/app/shared/services/position.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 
 import { Position } from "../models/position.model";
 import { Observable } from "rxjs";
+import { Message } from "../models/message.model";
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,17 @@ export class PositionService {
 
   fetch(categoryId: string): Observable<Array<Position>> {
     return this.http.get<Array<Position>>(`/api/position/${categoryId}`)
+  }
+
+  create(position: Position): Observable<Position> {
+    return this.http.post<Position>('/api/position', position)
+  }
+
+  update(position: Position): Observable<Position> {
+    return this.http.patch<Position>(`/api/position/${position._id}`, position)
+  }
+
+  delete(position: Position): Observable<Message> {
+    return this.http.delete<Message>(`/api/position/${position._id}`)
   }
 }
