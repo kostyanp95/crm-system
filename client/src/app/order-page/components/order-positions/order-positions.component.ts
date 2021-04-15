1820291bdc00be84fa33858d0457c956b4b3beb0
Kostyanp95 <kostyanp95@yandex.ru>
2021-04-15 03:41:28 +0300

    feat(order-position): create order position

---
+++ client/src/app/order-page/components/order-positions/order-positions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-positions',
+  templateUrl: './order-positions.component.html',
+  styleUrls: ['./order-positions.component.scss']
+})
+export class OrderPositionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
