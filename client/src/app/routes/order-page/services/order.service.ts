ed015b94236ce2958f997a0611052b6eedb195e9
Kostyanp95 <kostyanp95@yandex.ru>
2021-05-11 03:29:25 +0300

    feat(order-page/positions): implemented function add order

--- client/src/app/routes/order-page/services/order.service.ts
+++ client/src/app/routes/order-page/services/order.service.ts
@@ -1,19 +1,50 @@
 import { Injectable } from '@angular/core';
+import { Position } from '../../../shared/models/position.model';
+import { OrderPosition } from '../../../shared/models/order.model';
 
 @Injectable()
 export class OrderService {
 
-  constructor() { }
+  list: Array<OrderPosition> = []
+  price: number = 0
 
-  add(): void {
+  constructor() {
+  }
+
+  add(position: Position): void {
+    const orderPosition: OrderPosition = Object.assign({}, {
+      name: position.name,
+      coast: position.cost,
+      quantity: position.quantity,
+      _id: position._id
+    })
+
+    const candidate = this.list.find(p => p._id === position._id)
+
+    if (candidate) {
+      candidate.quantity += orderPosition.quantity
+    } else {
+      this.list.push(orderPosition)
+    }
+
+    this.computePrice()
 
   }
 
-  remove(): void {
-
+  remove(orderPosition: OrderPosition): void {
+    const index = this.list.findIndex(p => p._id === orderPosition._id)
+    this.list.splice(index, 1)
+    this.computePrice()
   }
 
   clear(): void {
 
   }
+
+  private computePrice() {
+    this.price = this.list.reduce((total, item) => {
+      return total += item.quantity * item.coast
+    }, 0)
+  }
+
 }
