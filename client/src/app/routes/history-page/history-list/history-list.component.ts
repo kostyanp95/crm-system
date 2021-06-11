a383c67588de8cf590d316554b3ec550f37e561a
Kostyanp95 <kostyanp95@yandex.ru>
2021-06-11 11:23:47 +0300

    feat(history-list.component): add view of orders

--- client/src/app/routes/history-page/history-list/history-list.component.ts
+++ client/src/app/routes/history-page/history-list/history-list.component.ts
@@ -1,18 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Order } from '../../../shared/models/order.model';
+import { MaterializeInstance, MaterializeService } from '../../../shared/services/materialize.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.scss']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
   @Input() orders: Array<Order>
+  @ViewChild('modal') modalRef: ElementRef
 
-  constructor() {
+  selectedOrder: Order
+  modal: MaterializeInstance
+
+  ngAfterViewInit(): void {
+    this.modal = MaterializeService.initModal(this.modalRef)
   }
 
-  ngOnInit(): void {
+  computePrice(order: Order): number {
+    return order.list.reduce((total, item) => {
+      return total += item.quantity * item.coast
+    }, 0)
   }
 
+  selectOrder(order: Order) {
+    this.selectedOrder = order
+    this.modal.open()
+  }
+
+  ngOnDestroy(): void {
+    this.modal.destroy()
+  }
+
+  closeModal(): void {
+    this.modal.close()
+  }
 }
