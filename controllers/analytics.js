1f916d9d258689ed89c70889285a3e6c1a5d5de9
Kostyanp95 <kostyanp95@yandex.ru>
2021-07-12 10:06:55 +0300

    feat(analytics.js): added calculation of analytics data

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,7 +1,97 @@
-module.exports.overview = function(req, res) {
+const moment = require('moment')
+const Order = require('../models/Order')
+const errorHandler = require('../utils/errorHandler')
+
+/**
+ * Страница Overview
+ * @param req - request
+ * @param res - response
+ */
+module.exports.overview = async function (req, res) {
+    try {
+        const allOrders = await Order.find({user: req.user.id}).sort(1)
+        const ordersMap = getOrdersMap(allOrders)
+        const yesterdayOrders = ordersMap[moment().add(-1, 'd').format('DD.MM.YYYY')] || []
+
+        // Количество заказов вчера
+        const yesterdayOrdersNumber = yesterdayOrders.length
+        // Количество заказов
+        const totalOrdersNumber = allOrders.length
+        // Количество дней
+        const daysNumber = Object.keys(ordersMap).length
+        // Заказов в день
+        const ordersPerDay = (totalOrdersNumber / daysNumber).toFixed(0)
+        // ((заказов вчера \ кол-во заказов в день) - 1) * 100
+        // Процент кол-ва заказов
+        const ordersPercent = (((yesterdayOrdersNumber / ordersPerDay) - 1) * 100).toFixed(2)
+        // Общая выручка
+        const totalGain = calculatePrice(allOrders)
+        // Выручка в день
+        const gainPerDay = totalGain / daysNumber
+        // Выручка за вчера
+        const yesterdayGain = calculatePrice(yesterdayOrders)
+        // Процент выручки
+        const gainPercent = (((yesterdayGain / gainPerDay) - 1) * 100).toFixed(2)
+        // Сравнение выручки
+        const compareGain = (yesterdayGain - gainPerDay).toFixed(2)
+        // Сравнение кол-ва заказов
+        const compareNumber = (yesterdayOrdersNumber - ordersPerDay).toFixed(2)
+
+        res.status(200).json({
+            gain: {
+                percent: Math.abs(+gainPercent),
+                compare: Math.abs(+compareGain),
+                yesterday: +yesterdayGain,
+                isHigher: +gainPercent > 0
+            },
+            orders: {
+                percent: Math.abs(+ordersPercent),
+                compare: Math.abs(+compareNumber),
+                yesterday: +yesterdayOrdersNumber,
+                isHigher: +ordersPercent > 0
+            }
+        })
+
+    } catch (error) {
+        errorHandler(res, error)
+    }
+}
+
+module.exports.analytics = function (req, res) {
 
 }
 
-module.exports.analytics = function(req, res) {
+/**
+ * Группировка по дням всех заказы.
+ * @param orders - список всех заказов.
+ */
+function getOrdersMap(orders = []): Array<Order> {
+    const daysOrders = {}
+    orders.forEach(order => {
+        const date = moment(order.date).format('DD.MM.YYYY')
 
+        if (date === moment().format('DD.MM.YYYY')) {
+            return
+        }
+
+        if (!daysOrders[date]) {
+            daysOrders[date] = []
+        }
+
+        daysOrders[date].push(order)
+    })
+
+    return orders
+}
+
+/**
+ * Рассчет общей выручки со всех заказов.
+ */
+function calculatePrice(orders = []) {
+    return orders.reduce((total, order) => {
+        const orderPrice = order.list.reduce((orderTotal, item) => {
+            return orderTotal += item.cost * item.quantity
+        }, 0)
+        return total += orderPrice
+    }, 0)
 }
