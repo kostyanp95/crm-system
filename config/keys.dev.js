ba7bb7a4c436f4ca029f478d73bc2d26ccd95c4e
Kostyanp95 <kostyanp95@yandex.ru>
2021-07-13 17:46:32 +0300

    Revert "feat(project): prepare for deploy"
    This reverts commit dbccc773

--- config/keys.dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://Kostyanp95:uM2xjKhbeCiyizRL@crm-system-shard-00-00.bcri7.azure.mongodb.net:27017,crm-system-shard-00-01.bcri7.azure.mongodb.net:27017,crm-system-shard-00-02.bcri7.azure.mongodb.net:27017/crm-system?ssl=true&replicaSet=crm-system-shard-0&authSource=admin&retryWrites=true&w=majority',
-    localMongoURI: 'mongodb://localhost/crm-system',
-    jwt: 'dev-jwt'
-}
