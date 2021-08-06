0e4edb38e612a9335abbcb7362962215e9922d9f
Kostyanp95 <kostyanp95@yandex.ru>
2021-08-06 23:24:05 +0300

    refactor(project): change configure

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'prod') {
+    console.log('KEYS PROD')
     module.exports = require('./keys.prod')
 } else if (process.env.NODE_ENV === 'dev') {
+    console.log('KEYS DEV')
     module.exports = require('./keys.dev')
 }
